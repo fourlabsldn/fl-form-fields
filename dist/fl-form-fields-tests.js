cdb16c12cdf8823c7ae3512aa3f72c46d162fb52
lazamar <lazaronijunior@gmail.com>
2017-01-10 17:42:03 +0000

    Add tests for addOption update

--- dist/fl-form-fields-tests.js
+++ dist/fl-form-fields-tests.js
@@ -33669,6 +33669,81 @@ function apiTest(field) {
 }
 
 /* eslint-env jasmine */
+describe("option-type update.addOption", function () {
+  var mockInitialState = function mockInitialState() {
+    return {
+      options: [{ caption: "placeholder" }]
+    };
+  };
+
+  var mockState = {
+    prop1: "prop1",
+    prop2: "prop2",
+    options: [{ caption: "default caption1" }, { caption: "default caption2" }, { caption: "default caption3" }],
+    newOptionCaption: ""
+  };
+
+  it("does not add a repeated option", function () {
+    var stateWithNewOptionRepeated = Object.assign({}, mockState, {
+      newOptionCaption: mockState.options[0].caption
+    });
+
+    var action = addOption(mockInitialState);
+    var newState = update(stateWithNewOptionRepeated, action);
+    expect(newState.options.length).toBe(stateWithNewOptionRepeated.options.length);
+  });
+
+  it("does not add an empty option", function () {
+    var stateWithNewOptionEmpty = Object.assign({}, mockState, {
+      newOptionCaption: ""
+    });
+
+    var action = addOption(mockInitialState);
+    var newState = update(stateWithNewOptionEmpty, action);
+    expect(newState.options.length).toBe(stateWithNewOptionEmpty.options.length);
+  });
+
+  it("Adds an option with a valid value", function () {
+    var caption = "A value";
+    var stateWithNewOptionValid = Object.assign({}, mockState, {
+      newOptionCaption: caption
+    });
+
+    var action = addOption(mockInitialState);
+    var newState = update(stateWithNewOptionValid, action);
+    expect(newState.options.length).toBeGreaterThan(stateWithNewOptionValid.options.length);
+    expect(newState.options[3].caption).toBe(caption);
+  });
+
+  it("Does not change other state propperties or options when adding one", function () {
+    var stateWithNewOptionValid = Object.assign({}, mockState, {
+      newOptionCaption: "A value"
+    });
+
+    var action = addOption(mockInitialState);
+    var newState = update(stateWithNewOptionValid, action);
+    expect(newState.options[0].caption).toBe(stateWithNewOptionValid.options[0].caption);
+    expect(newState.options[1].caption).toBe(stateWithNewOptionValid.options[1].caption);
+    expect(newState.options[2].caption).toBe(stateWithNewOptionValid.options[2].caption);
+    expect(newState.prop1).toBe(mockState.prop1);
+    expect(newState.prop2).toBe(mockState.prop2);
+  });
+
+  it("Removes default option when adding a new one", function () {
+    var caption = "A value";
+    var stateWithPlaceholder = Object.assign({}, mockState, {
+      options: mockInitialState().options,
+      newOptionCaption: caption
+    });
+
+    var action = addOption(mockInitialState);
+    var newState = update(stateWithPlaceholder, action);
+    expect(newState.options.length).toBe(1);
+    expect(newState.options[0].caption).toBe(caption);
+  });
+});
+
+/* eslint-env jasmine */
 describe("option-type update.updateOption", function () {
   var mockEvent = {
     target: {
--- tests/base-types/option-types/update/index.js
+++ tests/base-types/option-types/update/index.js
@@ -1,1 +1,2 @@
+import "./addOption";
 import "./updateOption";
